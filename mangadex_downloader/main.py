EngelPika32/mangadex-downloader

commit afa17dbde444b0e60f6abeee19350dc5b4b5573e

Fix failed to create manga info when downloading a cover art

mangadex_downloader/main.py
@@ -347,13 +347,6 @@ def download_cover_art_manga(url, replace=False):
     # To avoid error when using placeholders in --path option
     manga.chapters.language = Language(None)
 
-    if config.create_manga_info:
-        path = create_directory("", path=get_path(manga))
-        create_manga_info(path, manga, replace)
-
-        if config.manga_info_only:
-            return manga
-
     if cover.volume is None:
         cover_name = "No volume cover"
     else:
@@ -366,6 +359,13 @@ def download_cover_art_manga(url, replace=False):
     base_path = create_directory("", path=get_path(manga))
     path = base_path / filename
     log.info(f"Download directory is set to {base_path}")
+
+    if config.create_manga_info:
+        path = create_directory("", path=get_path(manga))
+        create_manga_info(path, manga, replace)
+
+        if config.manga_info_only:
+            return manga
 
     def handle_error(err, resp):
         if not resp.ok:

commit 39b344dbd4aa1018879da9f32581dc00a94ee254

fix: incorrect path causes subsequent crash during file removal
A regression caused by Commit afa17db changes the variable "path" unintended. This leads to the path pointing to the directory, and not the file. Subsequetly, the app crashes when trying to delete the directory, instead of the file.

The "path" here should be a second independent path.

Minified error, which is fixed by this commit:
line 150, in delete_file
    os.remove(file)
    ~~~~~~~~~^^^^^^
IsADirectoryError: [Errno 21] Is a directory

mangadex_downloader/main.py
@@ -361,8 +361,8 @@ def download_cover_art_manga(url, replace=False):
     log.info(f"Download directory is set to {base_path}")
 
     if config.create_manga_info:
-        path = create_directory("", path=get_path(manga))
-        create_manga_info(path, manga, replace)
+        path2 = create_directory("", path=get_path(manga))
+        create_manga_info(path2, manga, replace)
 
         if config.manga_info_only:
             return manga
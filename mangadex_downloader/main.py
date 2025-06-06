39b344dbd4aa1018879da9f32581dc00a94ee254
EngelPika32 <41995155+EngelPika32@users.noreply.github.com>
2025-06-06 10:00:16 +0200

    fix: incorrect path causes subsequent crash during file removal
    A regression caused by Commit afa17db changes the variable "path" unintended. This leads to the path pointing to the directory, and not the file. Subsequetly, the app crashes when trying to delete the directory, instead of the file.
    
    The "path" here should be a second independent path.
    
    Minified error, which is fixed by this commit:
    line 150, in delete_file
        os.remove(file)
        ~~~~~~~~~^^^^^^
    IsADirectoryError: [Errno 21] Is a directory

--- mangadex_downloader/main.py
+++ mangadex_downloader/main.py
@@ -361,8 +361,8 @@ def download_cover_art_manga(url, replace=False):
     log.info(f"Download directory is set to {base_path}")
 
     if config.create_manga_info:
-        path = create_directory("", path=get_path(manga))
-        create_manga_info(path, manga, replace)
+        path2 = create_directory("", path=get_path(manga))
+        create_manga_info(path2, manga, replace)
 
         if config.manga_info_only:
             return manga
